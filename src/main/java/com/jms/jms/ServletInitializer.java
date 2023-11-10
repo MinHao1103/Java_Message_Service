06df8eb52fabdb3f5bbf9c8606f5280e47a5a3fe
Ace <ace.sie.cwo@gmail.com>
2023-11-10 14:59:49 +0800

    [feat] init

---
+++ src/main/java/com/jms/jms/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.jms.jms;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(JmsApplication.class);
+    }
+
+}
